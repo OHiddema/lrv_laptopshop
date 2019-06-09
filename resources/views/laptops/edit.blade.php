a6b646902dff42c7ff932db5774f066731582d42
Oege Hiddema <oegehiddema@gmail.com>
2019-06-09 10:12:12 +0200

    Add form to update a laptop

--- resources/views/laptops/edit.blade.php
+++ resources/views/laptops/edit.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
    <h1>Edit laptop</h1>
+   <form method="POST" action="/laptops/{{$laptop->id}}">
+      {{method_field('PATCH')}}
+      {{csrf_field()}}
+      Brand: <input type="text" name='brand' value='{{$laptop->brand}}'><br><br>
+      Name: <input type="text" name='name' value='{{$laptop->name}}'><br><br>
+      Memory: <input type="text" name="memory" value='{{$laptop->memory}}'><br><br>
+      Price: <input type="number" name="price" value='{{$laptop->price}}'><br><br>
+      <button type="submit">Update</button>      
+   </form>
 @endsection
