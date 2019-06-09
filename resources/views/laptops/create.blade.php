1fdaf15915fe178c33175888a77b6b853b36e012
Oege Hiddema <oegehiddema@gmail.com>
2019-06-09 14:08:11 +0200

    Retain user input when validation fails

--- resources/views/laptops/create.blade.php
+++ resources/views/laptops/create.blade.php
@@ -10,10 +10,10 @@
    <h1>New laptop</h1>
    <form method="POST" action="/laptops">
       {{ csrf_field() }}
-      Brand: <input type="text" name='brand'><br><br>
-      Name: <input type="text" name='name'><br><br>
-      Memory: <input type="number" name="memory"><br><br>
-      Price: <input type="number" name="price"><br><br>
+      Brand: <input type="text" name='brand' value='{{old('brand')}}'><br><br>
+      Name: <input type="text" name='name' value='{{old('name')}}'><br><br>
+      Memory: <input type="number" name="memory" value='{{old('memory')}}'><br><br>
+      Price: <input type="number" name="price" value='{{old('price')}}'><br><br>
       <button type="submit">Add new laptop</button>
 
       @if($errors->any())
