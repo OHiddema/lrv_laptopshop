a54df4a147c2b37193d5a4c5057b5bb0c4926987
Oege Hiddema <oegehiddema@gmail.com>
2019-06-11 15:24:24 +0200

    only show prices history if there is at least one record

--- resources/views/laptops/show.blade.php
+++ resources/views/laptops/show.blade.php
@@ -6,12 +6,16 @@
    Memory: {{$laptop->memory}}<br>
    Price: {{$laptop->price}}<br>
    <br>
-   <!-- BEGIN Show price history -->
-   @foreach ($laptop->prices as $price)
-      <li>{{$price->startDate}}, {{$price->price}}</li>
-   @endforeach
-   <!-- END Show price history -->
-   <br>
-   
    <a href="/laptops/{{$laptop->id}}/edit">Edit laptop</a>
+   <br><br>
+
+   @if ($laptop->prices->count())
+      <div>
+         <h3>Price history</h3>
+         @foreach ($laptop->prices as $price)
+            <li>{{$price->startDate}}, {{$price->price}}</li>
+         @endforeach
+      </div>
+   @endif
+
 @endsection
